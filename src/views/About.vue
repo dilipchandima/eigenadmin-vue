1f345a03d8b5fe75aff2ec1c08802be7eb48b0b6
Chandima <chandima@rhino-partners.com>
2021-07-04 21:14:39 +0530

    trying some Vue basics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <input v-model="message" />
+    <p>{{ message }}</p>
+    <button class="btn btn-primary" v-on:click="reverseMessage">
+      Reverse Message
+    </button>
+    <ol class="list-group">
+      <li class="list-group-item" v-for="(todo, key) in todos" :key="key">
+        {{ todo.text }}
+      </li>
+    </ol>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TopNavigation",
+  data: function () {
+    return {
+      message: "You loaded this page on " + new Date().toLocaleString(),
+      todos: [
+        { text: "Learn JavaScript" },
+        { text: "Learn Vue" },
+        { text: "Build something awesome" },
+      ],
+    };
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+</script>
