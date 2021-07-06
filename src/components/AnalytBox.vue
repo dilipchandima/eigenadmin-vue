422a2bf30978ad6762ef1881c254d87de179cc60
Chandima <chandima@rhino-partners.com>
2021-07-06 14:06:16 +0530

    Add some styles to analyt box

--- src/components/AnalytBox.vue
+++ src/components/AnalytBox.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="col-md-3 col-sm-6 col-xs-12">
     <div class="analyt-container analyt-primary">
-      <span class="title">SESSIONS</span>
-      <span class="label">25</span>
-      <span class="description">2 sessions today</span>
+      <div class="content">
+        <span class="title">SESSIONS</span>
+        <span class="label">25</span>
+        <span class="description"
+          ><i class="bi bi-graph-up"></i> 2 sessions today</span
+        >
+      </div>
+      <div class="icon-container">
+        <i class="bi bi-person-lines-fill"></i>
+      </div>
     </div>
   </div>
 </template>
